feca30a84cf6c3b45d1399dd35aaf4ea83f47622
Adalberto Junior <adljunior@rd.com.br>
2022-07-07 20:09:33 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '26ccbd4d866a52f34c1869ce2171beb9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'adb8ca1b-684d-420d-9511-a3c046dfa78f'
+};
